4a122a24fbd04412235f8ff432c6224f8292a6b3
saakshipadamwar <90138650+saakshipadamwar@users.noreply.github.com>
2021-09-05 18:23:47 +0530

    Create camel_case.cpp

---
+++ camel_case.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+
+int main() {
+    string s;
+    cin>>s;
+    int count = 1;
+    for (const char c : s) {
+        if (c >= 'A' && c <= 'Z')
+            ++count;
+    }
+    cout<<count<<endl;
+}
